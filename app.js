e9e12e482b39ab33096649649c0d8f5156b202e3
sebokdas1 <sebok.das.ff@gmail.com>
2022-02-13 11:19:01 +0600

    case section worked

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('I am in a separate file')
+function caseNumber(isIncreasing) {
+    const inputText = document.getElementById('case-input');
+    const inputValue = inputText.value;
+    if (isIncreasing == true) {
+        inputText.value = parseInt(inputValue) + 1;
+    } else if (inputValue > 0) {
+        inputText.value = parseInt(inputValue) - 1;
+    };
+};
+document.getElementById('case-plus').addEventListener('click', function () {
+    caseNumber(true);
+});
+document.getElementById('case-minus').addEventListener('click', function () {
+    caseNumber(false);
+});
