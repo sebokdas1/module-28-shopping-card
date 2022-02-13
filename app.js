994ff8ac45ff9bb1033bcec5c88543a377325d5a
sebokdas1 <sebok.das.ff@gmail.com>
2022-02-13 11:37:34 +0600

    update case total

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 function caseNumber(isIncreasing) {
     const inputText = document.getElementById('case-input');
-    const inputValue = inputText.value;
+    let inputValue = inputText.value;
     if (isIncreasing == true) {
-        inputText.value = parseInt(inputValue) + 1;
+        inputValue = parseInt(inputValue) + 1;
     } else if (inputValue > 0) {
-        inputText.value = parseInt(inputValue) - 1;
+        inputValue = parseInt(inputValue) - 1;
     };
+    inputText.value = inputValue;
+    //update total
+    const total = document.getElementById('case-total');
+    total.innerText = inputValue * 59;
 };
 document.getElementById('case-plus').addEventListener('click', function () {
     caseNumber(true);
